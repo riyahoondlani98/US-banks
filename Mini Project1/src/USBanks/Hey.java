ed9159005ec89a9cc34d5c9e4857a1df5e266858
Riya Hoondlani <hoondlaniriya98@gmail.com>
2020-12-18 12:05:34 +0530

    changed 1st line from hello to namaste

--- Mini Project1/src/USBanks/Hey.java
+++ Mini Project1/src/USBanks/Hey.java
@@ -2,7 +2,7 @@ package USBanks;
 
 public class Hey {
 	public static void main(String[] args) {
-		System.out.println("Hello!");
+		System.out.println("Namaste");
 		System.out.println("Hi my name is Riya");
 		System.out.println("Welcome");
 	}
